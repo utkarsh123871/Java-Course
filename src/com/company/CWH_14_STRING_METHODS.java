20331ff4626218dd7933e044ad0aef10ffe64d2e
Utkarsh <utkarsh.gupta0311@gmail.com>
2022-04-02 19:57:03 +0530

    new commit

---
+++ src/com/company/CWH_14_STRING_METHODS.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class CWH_14_STRING_METHODS{
+    public static void main(String[] args) {
+        String name = "harry";
+        // String a1 = new String("harry");
+        // System.out.println(name);
+        // int value = name.length();
+        // System.out.print("Length of string is: ");
+        // System.out.println(value);
+        // String name1 = name.toLowerCase();
+        // System.out.println(name1);
+        // String name2 = name.toUpperCase();
+        // System.out.println(name2);
+
+        // String nonTrimmedtring = "    harry     ";
+        // System.out.println(nonTrimmedtring);
+        // System.out.println(nonTrimmedtring.trim());
+
+        // System.out.println(name.substring(2));
+        // // when end is mentioned then the end index is excluded but start is included
+        // System.out.println(name.substring(1 , 5));
+
+        // System.out.println(name.replace("RrY", "chutiya"));
+
+        // System.out.println(name.startsWith("ha"));
+        // System.out.println(name.endsWith("ry"));
+        
+        // System.out.println(name.charAt(0));
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.charAt(4));
+
+        // System.out.println(name.indexOf("r"));
+        // System.out.println(name.indexOf("r" , 3));
+
+        // System.out.println(name.equals("harry"));
+        // System.out.println(name.equals("haRry"));
+        // System.out.println(name.equalsIgnoreCase("haRry"));
+
+        System.out.println("I'm an escape sequence with \\ backslash");
+        System.out.println("I'm an escape sequence with \" double quote");
+        System.out.println("I'm an escape sequence with \' single quote");
+        System.out.println("I'm an escape sequence with \nnewline");
+    }
+}
